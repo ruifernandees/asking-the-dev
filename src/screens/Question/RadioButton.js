941adc5b3e46c96b5e10a3fded7697ffbd12a488
ruifernandees <dev.ruifernandes@gmail.com>
2020-06-02 16:22:24 -0300

    Question page created

---
+++ src/screens/Question/RadioButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RadioButton = () => (
+    <svg width="22" height="22" viewBox="0 0 22 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle className="outer" cx="11" cy="11.3436" r="11" fill="#FBEAFE"/>
+        <ellipse className="inner" cx="11" cy="11.5632" rx="9.16667" ry="8.97569" fill="white"/>
+    </svg>
+);
+
+export default RadioButton;
--- src/screens/Question/index.js
+++ src/screens/Question/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import RadioButton from './RadioButton';
+
+import './styles.css';
+import logo from '../../images/asking-the-dev.png';
+
 const Question = () => {
-    return <h1>Question</h1>;
+    return (
+        <section className="question">
+            <img className="logo" src={logo} alt="Asking the Dev logo"/>
+            <h1>What does CSS stand for?</h1>
+
+            <div className="cards">
+                <div className="card">
+                    <RadioButton />
+                    <p>Computing Style Sheet</p>    
+                </div>
+                <div className="card">
+                    <RadioButton />
+                    <p>Cascading Style Sheet</p>    
+                </div>
+                <div className="card">
+                    <RadioButton />
+                    <p>Colorful Style Sheet</p>    
+                </div>
+                <div className="card">
+                    <RadioButton />
+                    <p>Creative Style Sheet</p>    
+                </div>
+            </div>
+
+            <button>Send</button>
+        </section>
+    );
 };
 
 export default Question;
