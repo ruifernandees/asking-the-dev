9da07bf53967ff781112c4340b025fc30565e534
ruifernandees <dev.ruifernandes@gmail.com>
2020-06-08 17:44:47 -0300

    Hover and selected button stylization

--- src/screens/Question/index.js
+++ src/screens/Question/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RadioButton from './RadioButton';
 
@@ -6,25 +6,27 @@ import './styles.css';
 import logo from '../../images/asking-the-dev.png';
 
 const Question = () => {
+    const [selectedAnswer, setSelectedAnswer] = useState(0);
+
     return (
         <section className="question">
             <img className="logo" src={logo} alt="Asking the Dev logo"/>
             <h1>What does CSS stand for?</h1>
 
             <div className="cards">
-                <div className="card">
+                <div className="card selected" key="1" onPress={() => {}}>
                     <RadioButton />
                     <p>Computing Style Sheet</p>    
                 </div>
-                <div className="card">
+                <div className="card" key="2">
                     <RadioButton />
                     <p>Cascading Style Sheet</p>    
                 </div>
-                <div className="card">
+                <div className="card" key="3">
                     <RadioButton />
                     <p>Colorful Style Sheet</p>    
                 </div>
-                <div className="card">
+                <div className="card" key="4">
                     <RadioButton />
                     <p>Creative Style Sheet</p>    
                 </div>
